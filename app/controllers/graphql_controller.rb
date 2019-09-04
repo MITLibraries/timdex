fc98da29e166945fb0efad9349dda802b99390cd
Jeremy Prevost <jprevost@mit.edu>
2019-09-04 10:55:17 -0400

    more linting cleanup

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,6 +1,6 @@
 class GraphqlController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
@@ -9,10 +9,13 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
-    result = TimdexSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = TimdexSchema.execute(query, variables: variables,
+                                         context: context,
+                                         operation_name: operation_name)
     render json: result
-  rescue => e
+  rescue StandardError => e
     raise e unless Rails.env.development?
+
     handle_error_in_development e
   end
 
@@ -36,10 +39,11 @@ class GraphqlController < ApplicationController
     end
   end
 
-  def handle_error_in_development(e)
-    logger.error e.message
-    logger.error e.backtrace.join("\n")
+  def handle_error_in_development(err)
+    logger.error err.message
+    logger.error err.backtrace.join("\n")
 
-    render json: { error: { message: e.message, backtrace: e.backtrace }, data: {} }, status: 500
+    render json: { error: { message: err.message, backtrace: err.backtrace },
+                   data: {} }, status: :internal_server_error
   end
 end
