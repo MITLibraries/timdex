47e645d767fdf0a9ffe98ed7e3e8dd260aac7522
Jeremy Prevost <jprevost@mit.edu>
2019-10-23 11:10:54 -0400

    Refactor Retrieve to work with multiple promoted indexes
    This has no user facing changes.
    
    To date, we've only had one index "in production" at a time as we create or re-use the aleph_ indexes based on whether it is a full or partial ingest.
    
    Now that we are adding records from an additional source, we'll always have more than one production index... one for each source.
    
    This caused problems in how elasticsearch expects calls to retrieve a record work in that they require the index name to be passed, not the alias, in this situation.
    
    This code avoids that problem by doing a search instead of a retrieved when asking for a single record. It's kinda dumb, but it seems fine.

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -21,10 +21,11 @@ module Api
       end
 
       def record
-        @results = Timdex::EsClient.get(index: ENV['ELASTICSEARCH_INDEX'],
-                                        id: params[:id])
-      rescue Elasticsearch::Transport::Transport::Errors::NotFound
-        render json: { error: 'record not found' }.to_json, status: :not_found
+        f = to_filter(params[:id])
+        @results = Timdex::EsClient.search(index: ENV['ELASTICSEARCH_INDEX'], body: f)
+        if @results['hits']['total'] == 0
+          render json: { error: 'record not found' }.to_json, status: :not_found
+        end
       end
 
       def ping
@@ -33,6 +34,16 @@ module Api
 
       def ensure_json!
         request.format = :json
+      end
+
+      def to_filter(id)
+        {
+          query: {
+            ids: {
+              values: [id]
+            }
+        }
+      }.to_json
       end
     end
   end
--- app/views/api/v1/search/record.json.jbuilder
+++ app/views/api/v1/search/record.json.jbuilder
@@ -1,4 +1,4 @@
-result = @results['_source']
+result = @results['hits']['hits'][0]['_source']
 json.partial! partial: 'throttle'
 json.partial! partial: 'base', locals: { result: result }
 json.partial! partial: 'extended', locals: { result: result }
