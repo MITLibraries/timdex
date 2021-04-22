5001ac9697e1b4643b444e4520293e55d20ba434
Jeremy Prevost <jprevost@mit.edu>
2021-04-22 10:22:43 -0400

    Rails 6.1 update
    Followed the process as defined:
    https://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
