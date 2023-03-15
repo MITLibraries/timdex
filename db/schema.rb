7cb04afa245a50613e1de8190d0fa36d44d0d9d4
jazairi <adamjazairi@gmail.com>
2023-03-15 17:09:56 -0400

    Link to V2 dev docs from splash page
    Why these changes are being introduced:
    
    There is no clear path to the V2 docs from our splash page.
    
    Relevant ticket(s):
    
    https://mitlibraries.atlassian.net/browse/TIMX-209
    
    How this addresses that need:
    
    This adds a link to the V2 dev docs on the splash page.
    
    Side effects of this change:
    
    * The link in the deprecated docs paragraph now goes to the V2 docs,
    so I updated that to go to the deprecated docs page.
    * The db schema has been updated to include Rails 7 changes. (This
    PR does not require db migrations, but I had to run them because I
    am using a fresh clone of the repo.)

--- db/schema.rb
+++ db/schema.rb
@@ -2,31 +2,30 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_134246) do
-
+ActiveRecord::Schema[7.0].define(version: 2018_10_12_134246) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "locked_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
