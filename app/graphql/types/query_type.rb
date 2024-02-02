2bead08340d5eb51dac0ab2aab9200164387684d
Matthew Bernhardt <mjbernha@mit.edu>
2024-02-02 10:45:23 -0500

    Add filter for publication_information
    This is the input side of the equation only. Reporting back based on this field, as an aggregation, is part of a separate commit.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -71,6 +71,8 @@ module Types
                                                          'aggregation for a list of possible values'
       argument :literary_form_filter, String, required: false, default_value: nil,
                                               description: 'Filter results by fiction or nonfiction'
+      argument :publication_information_filter, [String], required: false, default_value: nil,
+                                                          description: 'Filter results by publication information'
       argument :source_filter, [String], required: false, default_value: nil,
                                          description: 'Filter by source record system. Use the `sources` aggregation ' \
                                                       'for a list of possible values'
@@ -129,6 +131,7 @@ module Types
       query[:contributors_filter] = filters[:contributors_filter]
       query[:languages_filter] = filters[:languages_filter]
       query[:literary_form_filter] = filters[:literary_form_filter]
+      query[:publication_information_filter] = filters[:publication_information_filter]
       query = source_deprecation_handler(query, filters[:source_filter], source)
       query[:subjects_filter] = filters[:subjects_filter]
       query
