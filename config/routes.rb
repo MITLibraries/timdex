91613914de9c18b85d10d19567ca12f779d531f6
Jeremy Prevost <JPrevost@users.noreply.github.com>
2026-07-15 10:53:28 -0400

    Add /up route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/graphql", to: "graphql#execute"
   devise_for :users
-  
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
   root to: 'static#index'
 
   namespace :api do
